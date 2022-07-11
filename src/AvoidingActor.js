393f01a7d6b5c7e8a9f5cbf234b57c051a1385b3
JeremyLeland <50390633+JeremyLeland@users.noreply.github.com>
2022-07-10 22:25:45 -0700

    Group avoids into larger cones

--- src/AvoidingActor.js
+++ src/AvoidingActor.js
@@ -3,26 +3,48 @@ import { Actor } from '../src/Actor.js';
 export class AvoidingActor extends Actor {
     
     target;
-    avoidList = [];
+
+    #avoidCones;
 
     getAvoidCones( entities ) {
-      return Array.from( entities, e => {
+      let cones = [];
+
+      entities.forEach( e => {
         const r = e.size;
         const cx = e.x - this.x;
         const cy = e.y - this.y;
         const h = Math.hypot( cx, cy );
         const angle = Math.atan2( cy, cx );
         const spread = Math.asin( Math.min( 1, r / h ) );   // prevent floating point errors when really close
+
+        const left  = fixAngle( angle - spread );
+        const right = fixAngle( angle + spread );
   
-        return {
-          left: fixAngle( angle - spread ),
-          right: fixAngle( angle + spread ),
-          dist: h,
-        }  
+        const collidingCones = cones.filter( cone => 
+          betweenAngles( left,  cone.left, cone.right ) ||
+          betweenAngles( right, cone.left, cone.right ) ||
+          betweenAngles( cone.left,  left, right ) ||
+          betweenAngles( cone.right, left, right )
+        );
+
+        if ( collidingCones.length > 0 ) {
+          cones = cones.filter( cone => !collidingCones.includes( cone ) );
+          cones.push( {
+            left:  Math.min( ...collidingCones.map( cone => cone.left ).concat( left ) ),
+            right: Math.max( ...collidingCones.map( cone => cone.right ).concat( right ) ),
+          } );
+        }
+        else {
+          cones.push( { left: left, right: right } );
+        }
       } );
+
+      return cones;
     }
 
-    update( dt ) {
+    update( dt, avoidList ) {
+      this.#avoidCones = this.getAvoidCones( avoidList );
+
       super.update( dt );
     }
 
@@ -35,60 +57,68 @@ export class AvoidingActor extends Actor {
 
     draw( ctx ) {
       super.draw( ctx );
-      
-      const avoidCones = this.getAvoidCones( this.avoidList );
   
       ctx.fillStyle = 'red';
       ctx.globalAlpha = 0.5;
-      avoidCones.forEach( cone => { 
+      this.#avoidCones.forEach( cone => { 
         ctx.beginPath();
         ctx.moveTo( this.x, this.y );
-        ctx.arc( this.x, this.y, cone.dist, cone.left, cone.right );
+        ctx.arc( this.x, this.y, 100, cone.left, cone.right );
         ctx.fill();
       } );
       ctx.globalAlpha = 1;
 
       if ( this.target ) {
+        const targetAngle = Math.atan2(
+          this.target.y - this.y, 
+          this.target.x - this.x 
+        );
+
+        const blockingCone = this.#avoidCones.find( cone => 
+          betweenAngles( targetAngle, cone.left, cone.right )
+        );
+
+        if ( blockingCone ) {
+          ctx.beginPath();
+          ctx.moveTo( this.x, this.y );
+          ctx.arc( this.x, this.y, 100, blockingCone.left, blockingCone.right );
+          ctx.fillStyle = 'red';
+          ctx.fill();
+        }
+
+        ctx.lineWidth = 2;
+
         ctx.beginPath();
         ctx.moveTo( this.x, this.y );
         ctx.lineTo( this.target.x, this.target.y );
         ctx.strokeStyle = 'yellow';
         ctx.stroke();
 
-        const targetAngle = Math.atan2(
-          this.target.y - this.y, 
-          this.target.x - this.x 
-        );
+        if ( blockingCone ) {  
+          const fromLeft = deltaAngle( blockingCone.left, targetAngle );
+          const fromRight = deltaAngle( targetAngle, blockingCone.right );
+          const angle = fromLeft < fromRight ? blockingCone.left : blockingCone.right;
 
-        const blockingCones = avoidCones.filter( e => 
-          deltaAngle( e.left, targetAngle ) > 0 && 
-          deltaAngle( targetAngle, e.right ) > 0 
-        );
-
-        ctx.fillStyle = 'red';
-        blockingCones.forEach( cone => { 
           ctx.beginPath();
           ctx.moveTo( this.x, this.y );
-          ctx.arc( this.x, this.y, cone.dist, cone.left, cone.right );
-          ctx.fill();
-        } );
+          ctx.lineTo( this.x + Math.cos( angle ) * 100, this.y + Math.sin( angle ) * 100 );
+          ctx.strokeStyle = 'lime';
+          ctx.stroke();
+        }
+
+        ctx.lineWidth = 1;
       }
     }
   }
 
-  // TODO: I'd like these to live in one place...can whatever we are
-  // using this for come from Cell instead?
-  export function fixAngle( a ) {
+  function fixAngle( a ) {
     return a > Math.PI ? a - Math.PI * 2 : a < -Math.PI ? a + Math.PI * 2 : a;
   }
 
-  export function deltaAngle( a, b ) {
+  function deltaAngle( a, b ) {
     return fixAngle( b - a );
   }
 
-  function clampAngle( angle, lower, upper ) {
-    const dLower = deltaAngle( lower, angle );
-    const dUpper = deltaAngle( angle, upper );
-
-    return dLower < 0 ? lower : dUpper < 0 ? upper : angle;
+  function betweenAngles( angle, lower, upper ) {
+    return 0 < deltaAngle( lower, angle ) && 0 < deltaAngle( angle, upper ); 
   }
