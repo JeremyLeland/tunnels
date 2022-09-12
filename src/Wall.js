92df1248f2d9c4991c090988c226379e53830b3e
Jeremy Leland <jeremy.leland@gmail.com>
2022-09-12 09:50:42 -0700

    Fill in walls with color

--- src/Wall.js
+++ src/Wall.js
@@ -4,12 +4,19 @@ import { WallInfo } from '../info/info.js';
 export class Wall extends Entity {  
   constructor( points ) {
     super( {}, Object.assign( { boundingPoints: points }, WallInfo.rock ) );
+
+    this.path = new Path2D();
+    points.forEach( p => this.path.lineTo( p[ 0 ], p[ 1 ] ) );
   }
 
   update( dt ) {
     // walls don't move
   }
 
-  // draw( ctx ) {
-  // }
+  drawEntity( ctx ) {
+    ctx.fillStyle = this.info.fillStyle;
+    ctx.fill( this.path );
+    ctx.strokeStyle = 'black';
+    ctx.stroke( this.path );
+  }
 }
