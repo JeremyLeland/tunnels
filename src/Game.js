ecd9687103c5a42bc03b74ac64e956a10780dad9
Jeremy Leland <jeremy.leland@gmail.com>
2022-10-25 20:53:31 -0700

    Forget the weird update-only thing and multiple canvases, go back to one canvas and drawing everything. I'll deal with slow rasterizing later.

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,14 @@
 export class Game {
-  keysPressed = new Set();
-  // mouse = { x: 0, y: 0, down: false, move: { x: 0, y: 0 } };
-
   scroll = { x: 0, y: 0 };
 
-  // #lastX;
-  // #lastY;
+  #ctx;
+  #reqId;
 
   constructor() {
     const canvas = document.createElement( 'canvas' );
     document.body.appendChild( canvas );
     
-    const ctx = canvas.getContext( '2d' );
+    this.#ctx = canvas.getContext( '2d' );
 
     canvas.oncontextmenu = () => { return false };
     window.onresize = () => {
@@ -19,58 +16,35 @@ export class Game {
       canvas.height = window.innerHeight * devicePixelRatio;
       canvas.style.width = window.innerWidth + 'px';
       canvas.style.height = window.innerHeight + 'px';
-      ctx.scale( devicePixelRatio, devicePixelRatio );
+      this.#ctx.scale( devicePixelRatio, devicePixelRatio );
     }
     window.onresize();
+  }
 
-    window.onkeydown = ( e ) => this.keysPressed.add( e.key );
-    window.onkeyup   = ( e ) => this.keysPressed.delete( e.key );
-
-    // const inputStart = ( e ) => {
-    //   this.mouse.down = true;
-    // }
-    // const inputMove = ( e ) => {
-    //   const event = e.touches ? e.touches[ 0 ] : e;
-
-    //   this.mouse.x = event.clientX;
-    //   this.mouse.y = event.clientY;
-    //   this.mouse.move.x = this.#lastX ? this.mouseX - this.#lastX : 0;
-    //   this.mouse.move.y = this.#lastY ? this.mouseY - this.#lastY : 0;
-    //   this.#lastX = this.mouse.x;
-    //   this.#lastY = this.mouse.y;
-    // }
-    // const inputStop = ( e ) => {
-    //   this.mouse.down = false;
-    //   this.#lastX = this.#lastY = undefined;
-    // }
-    // document.addEventListener( 'mousedown',  inputStart );
-    // document.addEventListener( 'touchstart', inputStart );
-    // document.addEventListener( 'mousemove', inputMove );
-    // document.addEventListener( 'touchmove', inputMove );
-    // document.addEventListener( 'mouseup',  inputStop );
-    // document.addEventListener( 'touchend', inputStop );
-    
-
-    let lastTime = null;
+  start() {
+    let lastTime;
     const animate = ( now ) => {
       lastTime ??= now;  // for first call only
       this.update( now - lastTime );
       lastTime = now;
   
-      ctx.clearRect( 0, 0, ctx.canvas.width, ctx.canvas.height );
+      this.#ctx.clearRect( 0, 0, this.#ctx.canvas.width, this.#ctx.canvas.height );
 
-      ctx.save();
-      ctx.translate( this.scroll.x, this.scroll.y );
-      this.draw( ctx );
-      ctx.restore();
+      this.#ctx.save();
+      this.#ctx.translate( this.scroll.x, this.scroll.y );
+      this.draw( this.#ctx );
+      this.#ctx.restore();
   
-      requestAnimationFrame( animate );
+      this.#reqId = requestAnimationFrame( animate );
     };
 
-    requestAnimationFrame( animate );
+    this.#reqId = requestAnimationFrame( animate );
+  }
+
+  stop() {
+    cancelAnimationFrame( this.#reqId );
   }
 
   update( dt ) {}
   draw( ctx ) {}
-
 }
