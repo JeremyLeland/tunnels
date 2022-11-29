358cee1014958d7cd9a1edde5167beeaec021559
Jeremy Leland <jeremy.leland@gmail.com>
2022-11-29 12:48:58 -0800

    Decent avoidance dodging

--- src/Actor.js
+++ src/Actor.js
@@ -7,8 +7,6 @@ import * as Util from './Util.js';
 export class Actor extends Entity {
   speed = 0;
 
-  // avoidVector = { x: 0, y: 0 };
-
   goalSpeed = 0;
   goalAngle = 0;
 
@@ -51,7 +49,7 @@ export class Actor extends Entity {
     const avoidVectors = [];
     const dodgeVectors = [];
 
-    const AVOID_DIST = 80, DODGE_DIST = 100;    // TODO: define elsewhere
+    const AVOID_DIST = 20, DODGE_DIST = 100;    // TODO: define elsewhere
 
     avoidList.forEach( other => {
       const p = this.getClosestPoints( other );
@@ -63,51 +61,12 @@ export class Actor extends Entity {
         y: -Math.sin( angle ) * repulsion,
       } );
 
-      // Distance from other's line of movement
-      const otherSpeed = Math.hypot( other.dx, other.dy );
-      if ( otherSpeed > 0 ) {
-        const otherVector = {
-          x: other.dx / otherSpeed,
-          y: other.dy / otherSpeed,
-        };
-        const cx = this.x - other.x;
-        const cy = this.y - other.y;
-        const front = cx * otherVector.x + cy * otherVector.y;
-        const side  = cx * otherVector.y - cy * otherVector.x;
-
-        // TODO: Maybe do this in both cases, to avoid running into someone from behind?
-        //if ( front > 0 ) {
-
-        const dist = 1;//Math.max( 0, 1 - front / DODGE_DIST );
-        const dodge = Math.max( 0, 1 - Math.abs( side ) / AVOID_DIST );
-
-        // TODO: We should dodge whichever way is closest to how we're already going (so we don't slow ourselves down!)
-        //       Calculate both side angles and use whichever is closer to front angle?
-        // But also need to account for if they are parallel
-
-        // TODO: Maybe emphasize the old dodge vector, but override it if too far away from our angle?
-
-        if ( this.angle == other.angle ) {  // TODO: Within epsilon?
-          dodgeVectors.push( {
-             x: otherVector.y * dist * dodge,
-             y: -otherVector.x * dist * dodge,
-          } );
-        }
-        else {
-          const dodge1 = other.angle - Math.PI / 2;
-          const dodge2 = other.angle + Math.PI / 2;
-
-          const bestDodge = 
-            Math.abs( Util.deltaAngle( dodge1, this.angle ) ) < 
-            Math.abs( Util.deltaAngle( dodge2, this.angle ) ) ? dodge1 : dodge2;
-        
-          dodgeVectors.push( {
-            x: Math.cos( bestDodge ) * dist * dodge,
-            y: Math.sin( bestDodge ) * dist * dodge,
-          } );
-        }
-
-        //}
+      // TODO: base this on their avoid cone somehow?
+      if ( Math.abs( Util.deltaAngle( this.angle, angle ) ) < 1 ) {  // TODO: what angle range?
+        avoidVectors.push( {
+          x:  Math.sin( angle ) * repulsion,
+          y: -Math.cos( angle ) * repulsion,
+        } );
       }
     } );
 
@@ -140,23 +99,23 @@ export class Actor extends Entity {
   draw( ctx ) {
     super.draw( ctx );
 
-    ctx.lineWidth = 2;
-    if ( this.#debug.goalVector ) {
-      ctx.strokeStyle = 'green';
-      drawVector( ctx, this.x, this.y, this.#debug.goalVector );
-    }
+    // ctx.lineWidth = 2;
+    // if ( this.#debug.goalVector ) {
+    //   ctx.strokeStyle = 'green';
+    //   drawVector( ctx, this.x, this.y, this.#debug.goalVector );
+    // }
 
-    this.#debug.avoidVectors?.forEach( avoidVector => {
-      ctx.strokeStyle = 'yellow';
-      drawVector( ctx, this.x, this.y, avoidVector );
-    } );
+    // this.#debug.avoidVectors?.forEach( avoidVector => {
+    //   ctx.strokeStyle = 'yellow';
+    //   drawVector( ctx, this.x, this.y, avoidVector );
+    // } );
 
-    this.#debug.dodgeVectors?.forEach( dodgeVector => {
-      ctx.strokeStyle = 'cyan';
-      drawVector( ctx, this.x, this.y, dodgeVector );
-    } );
+    // this.#debug.dodgeVectors?.forEach( dodgeVector => {
+    //   ctx.strokeStyle = 'cyan';
+    //   drawVector( ctx, this.x, this.y, dodgeVector );
+    // } );
 
-    ctx.lineWidth = 1;
+    // ctx.lineWidth = 1;
   }
 
   drawUI( ctx ) {
